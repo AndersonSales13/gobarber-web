692cf960dec3a2cca772491ed6d838292216b4ed
AndersonSales13 <salles_ti13@outlook.com>
2021-12-17 12:25:24 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,36 @@
+<h3 align="center">
+  Web Frontend created in ReactJS and TypeScript
+</h3>
+
+## 🥳 Overview
+
+Web Frontend of a Barber Shop Appointment System, development at Rocketseat's GoStack Bootcamp using ReactJS and TypeScript
+
+<!--
+## 🕵🏻‍♂️ Where find this tutorial video?
+
+- **[Princípios SOLID em uma API REST com Node.js e TypeScript | Code/Drops #44](https://www.youtube.com/watch?v=vAV4Vy4jfkc&t=8s)** -->
+
+## 👾 How to run this project in my machine?
+
+Install project dependencies with
+
+```bash
+npm install
+```
+
+or
+
+```bash
+yarn
+```
+
+After this, run following command for execute this project in your machine
+
+```bash
+yarn start
+```
+
+<!-- ## Routes of this application
+
+POST | http://localhost:3333/users - Create User -->
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,110 @@
+import React, { useCallback, useRef } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import api from '../../services/api';
+
+import { useToast } from '../../hooks/ToastContext';
+
+import { Container, Content, AnimationContainer, Background } from './styles';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const handleSubmit = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório'),
+          email: Yup.string()
+            .required('Email obrigatório')
+            .email('Digite um email válido'),
+          password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.post('/users', data);
+
+        addToast({
+          type: 'success',
+          title: 'Cadastro realizado!',
+          description: 'Você já pode fazer seu logon no GoBarber',
+        });
+
+        history.push('/');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro no cadastro',
+          description: 'Ocorreu um erro ao fazer o cadastro, tente novamente.',
+        });
+      }
+    },
+    [addToast, history],
+  );
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu cadastro</h1>
+            <Input type="name" name="name" placeholder="Nome" icon={FiUser} />
+            <Input
+              type="email"
+              name="email"
+              placeholder="E-mail"
+              icon={FiMail}
+            />
+            <Input
+              type="password"
+              name="password"
+              placeholder="Senha"
+              icon={FiLock}
+            />
+            <Button type="submit">Cadastrar</Button>
+          </Form>
+
+          <Link to="/">
+            <FiArrowLeft />
+            Voltar para logon
+          </Link>
+        </AnimationContainer>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
